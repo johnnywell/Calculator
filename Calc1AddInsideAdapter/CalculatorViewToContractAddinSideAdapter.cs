cba327e70078087fc45f0dc748e2e63d267faf38
Johnny Wellington dos Santos <johnny.w.santos@gmail.com>
2016-03-30 18:00:21 -0300

    Initial implementation
    It's missing the Add-In

---
+++ Calc1AddInsideAdapter/CalculatorViewToContractAddinSideAdapter.cs
@@ -0,0 +1,40 @@
+﻿using System.AddIn.Pipeline;
+using System.Runtime.Remoting.Channels;
+using CalcAddInViews;
+using CalculatorContracts;
+
+namespace CalcAddInsideAdapter
+{
+    // The AddInAdapterAttribute identifies this class as the add-in-side adapter
+    // pipeline segment.
+    [AddInAdapter()]
+    public class CalculatorViewToContractAddinSideAdapter : ContractBase, ICalc1Contract
+    {
+        private ICalculator _view;
+
+        public CalculatorViewToContractAddinSideAdapter(ICalculator view)
+        {
+            _view = view;
+        }
+
+        public double Add(double a, double b)
+        {
+            return _view.Add(a, b);
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return _view.Subtract(a, b);
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return _view.Multiply(a, b);
+        }
+
+        public double Divide(double a, double b)
+        {
+            return _view.Divide(a, b);
+        }
+    }
+}
---
+++ Calc1HVA/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcHVAs
+{
+    public interface ICalculator
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+}
